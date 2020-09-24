ffbc2dcbcfcd1fe03268547e8f88721e0557b43d
Krzysztof Kisiel <qminec@gmail.com>
2020-09-24 03:02:43 +0200

    Random numbers

--- src/Dice.java
+++ src/Dice.java
@@ -1,5 +1,31 @@
+
+package DiceGame;
+
+import java.util.Random;
+
+
 public class Dice {
+
+    private static int diceValue1;
+    private static int diceValue2;
+    private static int sum;
+
+
+
     public static void main(String[] args) {
 
+        Random r = new Random();
+
+        diceValue1 = r.nextInt(6)+1;
+        System.out.println("Value1: " + diceValue1);
+
+        diceValue2 = r.nextInt(6)+1;
+        System.out.println("Value2: " + diceValue2);
+
+        sum = diceValue1 + diceValue2;
+        System.out.println("Sum: " + sum);
     }
+
+
 }
+
